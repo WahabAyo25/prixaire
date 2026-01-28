84c5f8a0bee07a84a6f934f90e0936f0255886e8
Abubakar Abdulwahab <wahabayo1626@gmail.com>
2026-01-28 12:01:56 +0100

    Replace source with static export

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,18 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
+        protocol: "https",
+        hostname: "plus.unsplash.com",
       },
     ],
   },
